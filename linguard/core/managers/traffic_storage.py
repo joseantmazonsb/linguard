e062d16eca5aec84e7f2337f58d7b496dc828de7
Jose <joseantoniomazon@gmail.com>
2021-09-15 14:39:58 +0200

    #15 update workflows

--- linguard/core/managers/traffic_storage.py
+++ linguard/core/managers/traffic_storage.py
@@ -1,13 +1,11 @@
 from logging import warning
-from random import randint
-from typing import Dict, Any
+from typing import Dict
 
 from schedule import repeat, every
 
 from linguard.core.config.traffic import config
 from linguard.core.drivers.traffic_storage_driver import TrafficStorageDriver
 from linguard.core.drivers.traffic_storage_driver_json import TrafficStorageDriverJson
-from linguard.core.models import interfaces
 
 
 @repeat(every(1).hours)
@@ -16,27 +14,6 @@ def __update_data__():
         warning("Traffic's data collection is disabled!")
         return
     config.driver.save_data()
-
-
-def get_wireguard_traffic_mock() -> Dict[str, Any]:
-    dct = {}
-    for iface in interfaces.values():
-        rx = 0
-        tx = 0
-        for peer in iface.peers.values():
-            peer_rx = randint(1000, 9999999999)
-            peer_tx = randint(1000, 9999999999)
-            dct[peer.name] = {
-                "rx": peer_rx,
-                "tx": peer_tx
-            }
-            rx += peer_rx
-            tx += peer_tx
-        dct[iface.name] = {
-            "rx": rx,
-            "tx": tx
-        }
-    return dct
 
 
 registered_drivers: Dict[str, TrafficStorageDriver] = {}
