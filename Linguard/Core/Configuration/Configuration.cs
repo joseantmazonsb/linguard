65797d1f0b37c1924a9a02eeb330c242a237efea
José Antonio Mazón San Bartolomé <joseantoniomazon@gmail.com>
2022-02-27 02:04:05 +0100

    #115 refactor: clone logic for icloneable properties moved to utils class

--- Linguard/Core/Configuration/Configuration.cs
+++ Linguard/Core/Configuration/Configuration.cs
@@ -1,4 +1,6 @@
-﻿namespace Linguard.Core.Configuration; 
+﻿using Linguard.Core.Utils;
+
+namespace Linguard.Core.Configuration; 
 
 public class Configuration : IConfiguration {
     public IWireguardConfiguration Wireguard { get; set; } = new WireguardConfiguration();
@@ -7,15 +9,6 @@ public class Configuration : IConfiguration {
     public ITrafficConfiguration Traffic { get; set; } = new TrafficConfiguration();
     
     public object Clone() {
-        var clone = (IConfiguration) MemberwiseClone();
-        var cloneableProperties = GetType().GetProperties()
-            .Where(p => p.PropertyType.GetInterfaces().Contains(typeof(ICloneable)));
-        foreach (var property in cloneableProperties) {
-            var propertyValue = property.GetValue(this);
-            var propertyClone = propertyValue?.GetType().GetMethod(nameof(Clone))
-                !.Invoke(propertyValue, Array.Empty<object>());
-            property.SetValue(clone, propertyClone);
-        }
-        return clone;
+        return Cloning.Clone(this);
     }
 }
---
+++ Linguard/Core/Utils/Cloning.cs
@@ -0,0 +1,25 @@
+﻿namespace Linguard.Core.Utils; 
+
+public static class Cloning {
+    
+    /// <summary>
+    /// Clone an object with properties that implement <c>ICloneable</c>.
+    /// </summary>
+    /// <remarks>Properties that do not implement <c>ICloneable</c> won't be cloned.</remarks>
+    /// <remarks>The type must have a parameterless constructor.</remarks>
+    /// <param name="original"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static T Clone<T>(T original) where T : new() {
+        var clone = Activator.CreateInstance<T>();
+        var cloneableProperties = original!.GetType().GetProperties()
+            .Where(p => p.PropertyType.GetInterfaces().Contains(typeof(ICloneable)));
+        foreach (var property in cloneableProperties) {
+            var propertyValue = property.GetValue(original);
+            var propertyClone = propertyValue?.GetType().GetMethod(nameof(Clone))
+                !.Invoke(propertyValue, Array.Empty<object>());
+            property.SetValue(clone, propertyClone);
+        }
+        return clone;
+    }
+}
