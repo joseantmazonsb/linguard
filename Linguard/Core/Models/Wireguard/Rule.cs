9b45da3f97fd7def04b83f6b895b7e091e132a4d
José Antonio Mazón San Bartolomé <joseantoniomazon@gmail.com>
2022-03-01 23:05:27 +0100

    #115 chore: added tostring method to rule class

--- Linguard/Core/Models/Wireguard/Rule.cs
+++ Linguard/Core/Models/Wireguard/Rule.cs
@@ -3,10 +3,13 @@
 public class Rule {
     public string Command { get; set; }
     
-    public static implicit operator Rule(string command)
-    {
+    public static implicit operator Rule(string command) {
         return new Rule {
             Command = command
         };
+    }
+
+    public override string ToString() {
+        return Command;
     }
 }
